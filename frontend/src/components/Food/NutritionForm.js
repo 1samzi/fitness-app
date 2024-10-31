a4b0de441427733bdb6593aa95499b396c21efe8
vatsalbadhiwala <vatsalbadhiwala99@gmail.com>
2024-10-30 20:14:57 -0600

    add reset password

--- frontend/src/components/Food/NutritionForm.js
+++ frontend/src/components/Food/NutritionForm.js
@@ -1,14 +1,77 @@
+// import React from 'react'
+// import { Box } from '@chakra-ui/react'
+// import NavBar from '../NavBar'
+
+// const NutritionForm = () => {
+//   return (
+//     <Box>
+//         <NavBar />
+//         Nutrition Form (Note: Write your Nutrition components under this page)
+//     </Box>
+//   )
+// }
+
+// export default NutritionForm
+
 import React from 'react'
-import { Box } from '@chakra-ui/react'
+import {
+    Box,
+    Button,
+    Center,
+    Container,
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
+    Heading,
+    Input,
+    InputGroup,
+    InputRightElement,
+    VStack,
+    Text,
+    Link,
+    useToast,
+    Stack,
+  } from '@chakra-ui/react'
+
 import NavBar from '../NavBar'
 
+
 const NutritionForm = () => {
-  return (
+  return( 
     <Box>
-        <NavBar />
-        Nutrition Form (Note: Write your Nutrition components under this page)
+   
+      <NavBar></NavBar>
+        <Box w="75%" p={8} mt={10} borderWidth={1} borderRadius={8} boxShadow="lg" mx="auto">
+            <Center>
+                <Heading mb={6}>Track Your Meals</Heading>
+            </Center>
+            <Stack>
+                <Box>
+                    <Heading size = 'sm'>Breakfast</Heading>
+                </Box>
+                <Box  mt={8}>
+                    <Heading size = 'sm'>Lunch</Heading>
+                </Box>
+                <Box  mt={8}>
+                    <Heading size = 'sm'>Dinner</Heading>
+                </Box>
+                <Box  mt={8}>
+                    <Heading size = 'sm'>Snacks</Heading>
+                </Box>
+            </Stack>
+            <FormControl mt={8}>
+                <InputGroup>
+                    <Input placeholder="Search for food...">
+                        
+                    </Input>
+                </InputGroup>
+            </FormControl>
+            
+        </Box>
+   
     </Box>
-  )
+    
+)
 }
 
 export default NutritionForm
